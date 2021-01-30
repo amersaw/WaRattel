89eface7eb11ee807b5bfacd258a295220935ba5
Amer Sawan <amersaw@gmail.com>
2021-01-30 10:21:59 +0300

    fix syntax err

--- model/predef_option.py
+++ model/predef_option.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class PredefOption:
     def __init__(self, title: str, command: str, pos_row: int, pos_column: int):
         self.title = title
@@ -7,7 +10,7 @@ class PredefOption:
         pass
 
     @classmethod
-    def KB_PrevNext() -> list["PredefOption"]:
+    def KB_PrevNext() -> List["PredefOption"]:
         res: list[PredefOption] = []
         res.append(PredefOption("Next", "next", 0, 0))
         res.append(PredefOption("Previous", "prev", 0, 1))
